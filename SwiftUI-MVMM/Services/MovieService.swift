778d0e07f7c9939b9be188bd09b7647a8a0e8b73
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-18 20:35:13 +0300

    Service Created.

---
+++ SwiftUI-MVMM/Services/MovieService.swift
@@ -0,0 +1,28 @@
+//
+//  MovieService.swift
+//  SwiftUI-MVMM
+//
+//  Created by rico on 18.12.2025.
+//
+
+import Foundation
+
+protocol MovieServiceProtocol {
+    func fetchMovies(page: Int) async throws -> [MovieResponseModel]
+}
+
+final class MovieService: MovieServiceProtocol {
+    private let networkManager: NetworkManagerProtocol
+    
+    init(networkManager: NetworkManagerProtocol = NetworkManager()) {
+        self.networkManager = networkManager
+    }
+    
+    
+    func fetchMovies(page: Int) async throws -> [MovieResponseModel] {
+        let endPoint = MoviesEndPoint.discover(page: page)
+        let response = try await networkManager.request(endpoint: endPoint, type: MovieGeneralResponse.self)
+        
+        return response.results ?? []
+    }
+}
