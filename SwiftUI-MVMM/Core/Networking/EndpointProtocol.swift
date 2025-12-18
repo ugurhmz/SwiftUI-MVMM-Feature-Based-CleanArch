58a0f99049e5e53f449a66ff499fe2845aa87c89
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-18 16:50:11 +0300

    Alamofire & EndpointProtocol

--- SwiftUI-MVMM/Core/Networking/EndpointProtocol.swift
+++ SwiftUI-MVMM/Core/Networking/EndpointProtocol.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import Alamofire
+
+protocol EndPointProtocol {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: HTTPHeader? { get }
+    var paramters: Parameters? { get }
+    var encoding: ParameterEncoding { get }
+}
+
+extension EndPointProtocol {
+    var baseURL: String {
+        return APIConstants.baseURL
+    }
+    
+    var headers: HTTPHeaders? {
+        return [
+            "Authorization": "Bearer \(APIConstants.bearerToken)",
+            "accept" : "application/json"
+        ]
+    }
+    
+    var encoding: ParameterEncoding {
+        return URLEncoding.default
+    }
+}
