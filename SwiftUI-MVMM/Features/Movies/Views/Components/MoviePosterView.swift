1f0343152025368af6f56ba620dc42537482cc64
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-18 20:59:53 +0300

    MoviePosterView component created.

--- SwiftUI-MVMM/Features/Movies/Views/Components/MoviePosterView.swift
+++ SwiftUI-MVMM/Features/Movies/Views/Components/MoviePosterView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct MoviePosterView: View {
+    let url: URL?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        AsyncImage(url: url) { phase in
+            switch phase {
+            case .empty:
+                Rectangle()
+                    .fill(Color.gray.opacity(0.5))
+                    .overlay(ProgressView())
+            case .success(let image):
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            case .failure(let error):
+                ZStack {
+                    Color.gray.opacity(0.1)
+                    Image(systemName: "photo.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(.tertiary)
+                    
+                }
+            @unknown default:
+                EmptyView()
+            }
+        }
+        .frame(width: 80, height: 120)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
+        
     }
 }
 
 #Preview {
-    MoviePosterView()
+    MoviePosterView(url: URL(string: ""))
 }
