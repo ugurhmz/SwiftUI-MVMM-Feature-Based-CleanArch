f60b983965b60a0eb0de52d2138b1023401a15bd
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-19 20:09:29 +0300

    KingFisher added for Images.

--- SwiftUI-MVMM/Features/Movies/Views/Components/MoviePosterView.swift
+++ SwiftUI-MVMM/Features/Movies/Views/Components/MoviePosterView.swift
@@ -6,36 +6,42 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct MoviePosterView: View {
     let url: URL?
     
     var body: some View {
-        AsyncImage(url: url) { phase in
-            switch phase {
-            case .empty:
+        KFImage(url)
+            .placeholder {
                 Rectangle()
-                    .fill(Color.gray.opacity(0.5))
-                    .overlay(ProgressView())
-            case .success(let image):
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-            case .failure(_):
-                ZStack {
-                    Color.gray.opacity(0.1)
-                    Image(systemName: "photo.fill")
-                        .font(.largeTitle)
-                        .foregroundStyle(.tertiary)
-                    
-                }
-            @unknown default:
-                EmptyView()
+                    .fill(Color.black.opacity(0.3))
+                    .overlay(
+                            ProgressView()
+                                .tint(.green)
+                    )
             }
-        }
-        .frame(width: 80, height: 120)
-        .clipShape(RoundedRectangle(cornerRadius: 12))
-        .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
+            .fade(duration: 0.25)
+            .onFailure { error in
+                print("Resim yükleme hatası: \(error)")
+            }
+            .resizable()
+            .loadDiskFileSynchronously()
+            .cacheOriginalImage()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 100, height: 150)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .shadow(color:.black.opacity(0.2), radius: 4, x: 0, y: 2)
+            .background(
+                ZStack {
+                    if url == nil {
+                        Color.gray.opacity(0.1)
+                        Image(systemName: "photo.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.tertiary)
+                    }
+                }
+            )
         
     }
 }
