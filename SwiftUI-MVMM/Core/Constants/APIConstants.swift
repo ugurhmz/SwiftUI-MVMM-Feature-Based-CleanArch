5714a20131408865adb75c704b2eb70222f85289
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-19 18:33:19 +0300

    Secrets.xcconfig added to this project.
    Secrets.xcconfig for TMDB_TOKEN & Info.plist.

--- SwiftUI-MVMM/Core/Constants/APIConstants.swift
+++ SwiftUI-MVMM/Core/Constants/APIConstants.swift
@@ -7,7 +7,17 @@
 
 import Foundation
 
+enum SecretKeys: String {
+    case TMDB_TOKEN = "TMDB_TOKEN"
+}
+
 struct APIConstants {
     static let baseURL = "https://api.themoviedb.org/3"
-    static let bearerToken = ""
+    static var bearerToken: String {
+        guard let token = Bundle.main.object(forInfoDictionaryKey: SecretKeys.TMDB_TOKEN.rawValue) as? String else {
+            fatalError("HATA: Secrets.xcconfig dosyası bulunamadı veya Info.plist ayarı eksik!")
+        }
+        
+        return token
+    }
 }
